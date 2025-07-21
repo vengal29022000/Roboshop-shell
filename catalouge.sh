6d3cb82597f3efe129f3c6a616b4855a77c94ad0
gowthamflm <vengalareddy.challa@gmail.com>
2025-07-21 12:36:02 +0530

    catlogue9

--- catalouge.sh
+++ catalouge.sh
@@ -5,7 +5,7 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-MONGDB_HOST=mongodb.daws76s.online
+MONGDB_HOST=mogodb.daws76s.online
 
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
